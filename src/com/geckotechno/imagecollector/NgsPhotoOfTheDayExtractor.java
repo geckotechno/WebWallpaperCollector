c02ea1038568a21dc86a2b4966ca062c6acb795e
Michael Hale <geckotechno@yahoo.com>
2014-12-19 12:38:28 -0800

    Improved performance of extract by storing the URL of where the image is extract in extract list instead of the actuall image name.  Removes the need to parse the page for the image name.

--- src/com/geckotechno/imagecollector/NgsPhotoOfTheDayExtractor.java
+++ src/com/geckotechno/imagecollector/NgsPhotoOfTheDayExtractor.java
@@ -15,10 +15,12 @@ public class NgsPhotoOfTheDayExtractor extends ImageCollectorFromHtml {
     }
 
     protected static void extractFromNGS() throws Exception {
-        File dir = new File("C:\\Users\\hale\\Wallpaper-web\\ngs-incoming");
+        File dir = new File(System.getenv("USERPROFILE"), "Pictures\\Wallpaper\\Wallpaper-web\\ngs-incoming");
+        if (! dir.exists()) {
+            dir.mkdirs();
+        }
         Set<String> excludeSet = getPreviousExtractNames(dir);
-        String[] categories = new String[]{ "underwater","science-space","landscapes","nature-weather",
-                                                "black-white","adventure-exploration"};
+        String[] categories = new String[]{ "underwater","science-space","landscapes","nature-weather","black-white","adventure-exploration"};
         for (int i = 0; i < categories.length; i++ ) {
             System.out.println("NGS searching: " + categories[i]);
             URL url = new URL("http://photography.nationalgeographic.com/photography/photo-of-the-day/" + categories[i] +"/");
@@ -27,13 +29,13 @@ public class NgsPhotoOfTheDayExtractor extends ImageCollectorFromHtml {
         writeExtractFileSet(dir,excludeSet);
     }
 
-    private static Set<String> downloadnNatGeoPhotoOfTheDay(URL url, File dir, Set<String> excludeFiles) throws Exception {
-        Set<String> newFiles = new HashSet<>();
+    private static Set<String> downloadnNatGeoPhotoOfTheDay(URL url, File dir, Set<String> excludeUrls) throws Exception {
+        Set<String> newUrls = new HashSet<>();
         Set<String> links = downloadLinksFromURL(url);
 //        System.out.println("links:\n" + outputList(links));
         if (links == null) {
           System.out.println("--Error getting files");
-          return newFiles;
+          return newUrls;
         }
 
         Set<String> imgFolderLinks = filterImgFolderLinks(links);
@@ -47,26 +49,31 @@ public class NgsPhotoOfTheDayExtractor extends ImageCollectorFromHtml {
 
         for (String childUrl: imgFolderLinks ) {
 //            System.out.println("\n\ngetfirst url: " + childUrl);
-            Set<String> childLinks = downloadLinksFromURL(new URL(childUrl));
-            if (childLinks != null) {
-                String childImg = findImgLInk(childLinks, getUrlTail(childUrl));
-                //        System.out.println("links:\n" + outputList(parseImgLinks(childLinks)));
-                //            System.out.println("child img link: " + childImg);
-                if (childImg != null && childImg.length() > 20 && fileNameNotMatch(childImg, excludeFiles)) {
-                    System.out.print(DOWNLOAD_FILE_CHAR);
-                    downloadImagesFromURL(childImg, dir);
-                    newFiles.add(getBaseName(childImg));
-                } else {
-                    System.out.print(SKIP_FILE_CHAR);
-                }
+            if (excludeUrls.contains(childUrl)) {
+                System.out.print(SKIP_FILE_CHAR);
             } else {
-                System.out.print(ERROR_CHAR);
+                Set<String> childLinks = downloadLinksFromURL(new URL(childUrl));
+                if (childLinks != null) {
+                    String childImg = findImgLInk(childLinks, getUrlTail(childUrl));
+                    //        System.out.println("links:\n" + outputList(parseImgLinks(childLinks)));
+                    //            System.out.println("child img link: " + childImg);
+                    if (childImg != null && childImg.length() > 20) {
+                        System.out.print(DOWNLOAD_FILE_CHAR);
+                        downloadImagesFromURL(childImg, dir);
+                        newUrls.add(childUrl);
+                    } else {
+                        newUrls.add(childUrl);
+                        System.out.print(SKIP_FILE_CHAR);
+                    }
+                } else {
+                    System.out.print(ERROR_CHAR);
+                }
             }
 
         }
         System.out.println();
 
-        return newFiles;
+        return newUrls;
     }
 
     private static void downloadnFlicker(URL url, File dir) throws Exception {
